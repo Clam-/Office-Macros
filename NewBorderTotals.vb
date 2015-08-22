06163364b3c6fb3004d5959cd7e2ae789fbe4430
Clam <clam@nyanya.org>
2015-08-22 22:03:54 +1000

    Added new Border method:
    * Attempts to line the borders up with existing borders

---
+++ NewBorderTotals.vb
@@ -0,0 +1,54 @@
+Sub NewBorders()
+     ' xlEdgeRight xlContinuous
+    Dim hasTotal As Boolean
+    Dim cellStart, cellEnd As String
+    Dim bordStart, boardEnd As String
+    Dim borderRange As Range
+    Dim r As Integer
+    
+    For Each Row In ActiveSheet.UsedRange.Rows
+        hasTotal = False
+        bordStart = ""
+        bordEnd = ""
+        For Each cell In Row.Cells
+            ' detect existing borders
+            If cell.Borders(xlEdgeRight).LineStyle = xlContinuous Then
+                If bordStart <> "" Then
+                    bordEnd = cell.Address
+                End If
+            End If
+            If cell.Borders(xlEdgeLeft).LineStyle = xlContinuous Then
+                If bordStart = "" Then
+                    bordStart = cell.Address
+                End If
+            End If
+            ' Detect cell contents
+            If Not IsEmpty(cell) Then
+                If cell.Value2 Like "*Total*" Then
+                    hasTotal = True
+                    cellStart = cell.Address
+                    cellEnd = cell.Address
+                End If
+            End If
+            If hasTotal Then
+                If IsEmpty(cell) Then
+                    ' pass
+                Else
+                    cellEnd = cell.Address
+                End If
+            End If
+        Next
+        
+        If hasTotal Then
+            If bordEnd <> "" Then
+                cellStart = bordStart
+                cellEnd = bordEnd
+            End If
+            Set borderRange = ActiveSheet.Range(cellStart, cellEnd)
+            borderRange.Borders.LineStyle = xlContinuous
+            borderRange.Borders.Weight = xlMedium
+            borderRange.Borders.Color = RGB(0, 0, 0)
+            borderRange.Borders(xlInsideVertical).LineStyle = xlLineStyleNone
+        End If
+    Next
+End Sub
